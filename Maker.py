b345cfa90a53c9b2266a002129a3391c8d1915c9
tjpain13 <87914410+tjpain13@users.noreply.github.com>
2021-07-24 22:05:32 -0400

    Update Maker.py

--- Maker.py
+++ Maker.py
@@ -5,7 +5,8 @@ import datetime
 
 # Pull HTML from finviz channel up page, parse
 url = "https://finviz.com/screener.ashx?v=150&s=ta_p_channelup"
-headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:78.0) Gecko/20100101 Firefox/78.0"}
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) "
+                         "Version/14.0.3 Safari/605.1.15"}
 request = urllib.request.Request(url, headers=headers)
 response = urllib.request.urlopen(request)
 html = response.read()
