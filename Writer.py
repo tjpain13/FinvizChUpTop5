d51440a1d5a2b0aaadff4b57fa8223d2c0f89e0f
tjpain13 <87914410+tjpain13@users.noreply.github.com>
2022-04-25 14:31:05 -0400

    Update Writer.py
    comments, formatting

--- Writer.py
+++ Writer.py
@@ -1,5 +1,5 @@
-# Go through files in /Your/Path/Here/ add the ohlccv data if file is less than
-# 21 days old, or move to /Your/Path/Here/.Old if file is 21 or more days old
+# Go through files in {filepath} add the ohlccv data if file is less than
+# 20 days old, or move to {filepath}/Old if file is 21 or more days old
 
 import linecache
 import os
@@ -10,18 +10,26 @@ from shutil import move
 
 
 curdt = datetime.date.today()
-path = "/Your/Path/Here/"  # add your desired path here, INCLUDE trailing "/" or "\"
+path = ""  # add your desired path inside the quotes, INCLUDE trailing "/" or "\"
 
 for d in os.listdir(f'{path}'):
+
+    # Disregard old folder and other hidden folders
     if not d.startswith('.'):
+
+        # Isolate date of file creation
         secondline = linecache.getline(f'{path}{d}', 2).rstrip('\n')
         oldday = datetime.datetime.strptime(secondline, "%Y-%m-%d").date()
         dayselapsed = curdt - oldday
+
+        # If file is less than 21 days old, write data
         if dayselapsed < datetime.timedelta(days=21):
             url = f'https://finance.yahoo.com/quote/{PurePath(d).stem}/history?p={PurePath(d).stem}'
             stockfile = open(f'{path}{d}', 'a')
             stockfile.write(f'\n{ohlccv_output(url)}')
             stockfile.close()
+
+        # Otherwise, move file to /Old directory, making /Old directory if needed
         elif dayselapsed >= datetime.timedelta(days=21):
             if os.path.isdir(f'{path}.Old') is True:
                 move(f'{path}{d}', f'{path}.Old')
